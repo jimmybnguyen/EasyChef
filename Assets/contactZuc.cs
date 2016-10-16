15c30e8b0455ca13ccd33331620e12c4239f61a6
jimmybnguyen <jimmyn9@uw.edu>
2016-10-16 13:55:10 -0700

    add zuc

---
+++ Assets/contactZuc.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+
+public class contactZuc : MonoBehaviour
+{
+    SteamVR_TrackedObject trackedObj;
+    public GameObject zucSlices;
+    public GameObject soundfx;
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "zuchinni")
+        {
+            Instantiate(soundfx, col.transform.position, Quaternion.identity);
+            Destroy(col.gameObject);
+            for (int i = 0; i < 5; i++)
+            {
+                Instantiate(zucSlices, col.transform.position, Quaternion.identity);
+            }
+        }
+    }
+
+}
+
