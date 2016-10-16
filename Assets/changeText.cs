aa43b3e36a65c3462f99e9d47501b533fb2ea375
jimmybnguyen <jimmyn9@uw.edu>
2016-10-16 14:07:38 -0700

    add final food

--- Assets/changeText.cs
+++ Assets/changeText.cs
@@ -9,8 +9,8 @@ public class changeText : MonoBehaviour {
     TextMesh instruction;
     int i = 0;
     string[] instructions = { "Hi! Welcome to EasyChef.\nToday we'll help you make some\nzucchini linguine. Touch the\n'green box to get your next instructions\nand the red box to go back",
-                              "First, touch & grip the\nzucchini to peel it",
-                              "Touch & grip the bell \npepper to slice it!",
+                              "First, touch the\nzucchini to peel it",
+                              "Touch \npepper to slice it!",
                               "Touch & grip the lime \nto juice it",
                               "Toss it all in the tray",
                               "Pop it on into the oven",
