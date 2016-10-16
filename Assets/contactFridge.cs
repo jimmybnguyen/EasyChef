ff408508a67005a20dde88c56afb265e14bd9e7e
jimmybnguyen <jimmyn9@uw.edu>
2016-10-15 22:24:45 -0700

    add fridge and display to scene

---
+++ Assets/contactFridge.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class contactFridge : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
