cf997814e2fb8e6c318dc4d44d231c99662527bb
jimmybnguyen <jimmyn9@uw.edu>
2016-10-16 07:55:30 -0700

    oven stuff

--- Assets/Openable_Oven.cs
+++ Assets/Openable_Oven.cs
@@ -1,81 +1,72 @@
 ﻿using UnityEngine;
-using System.Collections;
+using VRTK;
 
 public class Openable_Oven : VRTK_InteractableObject {
 
 	public bool opened = false;
+    public bool flipped = false;
+    public bool rotated = false;
 
-	private float maxRotationX = 70.0F;
-	private float maxPosZ = -1.5F;
-	private float maxPosY = -2.0F;
-	private float turnspeed = 17.5F;
-	private float movespeedy = 0.5F;
-	private float movespeedz = 0.5F;
+    private float speed = 100.0F;
+    private float sideFlip = -1;
+    private float side = -1;
+    private float doorOpenAngle = -70.0F;
 
-	public override void StartUsing(GameObject usingObject)
+    private Vector3 defaultRotation;
+    private Vector4 openRotation;
+    private Vector3 openPosition;
+    private Vector3 defaultPosition;
+
+
+ //   private float maxRotationX = 70.0F;
+ ////private float maxPosZ = -1.5F;
+ ////private float maxPosY = -2.0F;
+ //private float turnspeed = 17.5F;
+ //private float movespeedy = 0.5F;
+ //private float movespeedz = 0.5F;
+
+    public override void StartUsing(GameObject usingObject)
 	{
 		base.StartUsing(usingObject);
-		opened = !opened;
-		//the currently closed door will open
-		//once the method is called
-
+        SetDoorRotation(usingObject.transform.position);
+        SetRotation();
+        SetPosition();
+        opened = !opened;
 	}
 
 	// Use this for initialization
-	void Start () {
-		base.Start ();
-	}
+	protected override void Start () {
+		base.Start();
+        defaultRotation = transform.eulerAngles;
+        defaultPosition = transform.position;
+        SetRotation();
+        SetPosition();
+        sideFlip = (flipped ? 1 : -1);
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		if(opened)
-		{
-			openOven();
-
-		}else
-		{
-			closeOven();	
-			
-		}
-	}
-	void openOven()
-	{
-		if (transform.eulerAngles.x < maxRotationX) {
-			//right is x, stop rotating til we are at 70 degrees
-			transform.Rotate (Vector3.right, turnspeed * Time.deltaTime, Space.Self);
-		}
-
-		if (transform.localPosition.y > maxPosY) {
-			//up is y
-			transform.Translate (Vector3.down * Time.deltaTime * movespeedy, Space.World);
-		}
-
-		float percent = transform.eulerAngles.x / maxRotationX;
-		if (percent >= 0.25 && transform.localPosition.z > maxPosZ) {
-			//forward is z
-			transform.Translate (Vector3.back * Time.deltaTime * movespeedz, Space.World);
-			//it takes the z animation a second before execution
-		}
+	protected override void Update () {
+		if(opened) {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(openRotation), Time.deltaTime * speed);
+            transform.position = Vector3.MoveTowards(transform.position, openPosition, Time.deltaTime * speed);
+		} else {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(defaultRotation), Time.deltaTime * speed);
+            transform.position = Vector3.MoveTowards(transform.position, defaultPosition, Time.deltaTime * speed);
+        }
 	}
 
-	void closeOven()
-	{
-		if (transform.eulerAngles.x > 0) {
-			//right is x
-			//transform.eulerAngles is the current angle of the door
-			transform.Rotate (Vector3.left, turnspeed * Time.deltaTime, Space.Self);
-		}
+    private void SetRotation()
+    {
+        openRotation = new Vector3(defaultRotation.x + (doorOpenAngle * (sideFlip * side)), defaultRotation.y, defaultRotation.z);
+    }
 
-		if (transform.localPosition.y < 0) {
-			//up is y
-			transform.Translate (Vector3.up * Time.deltaTime * movespeedy, Space.World);
-		}
+    private void SetPosition()
+    {
+        openPosition = new Vector3(defaultPosition.x, defaultPosition.y + -0.4F, defaultPosition.z + -0.35F);
+    }
 
-		float percent = transform.eulerAngles.x / maxRotationX;
-		//70/70 is one, it will start earlier. it finishes a minute ealrier than starts ealier
-		if (percent >= 0.25 && transform.localPosition.z < 0) {
-			//forward is z
-			transform.Translate (Vector3.forward * Time.deltaTime * movespeedz, Space.World);
-		}
-	}
+    private void SetDoorRotation(Vector3 interacterPosition)
+    {
+        side = ((rotated == false && interacterPosition.z > transform.position.z) || (rotated == true && interacterPosition.y > transform.position.y) ? -1 : 1);
+    }
 }
